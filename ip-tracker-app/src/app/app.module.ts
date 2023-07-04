0f52d893aaca0259dbb13470f0228b86c415b923
Eugen Andrei Vrabie <v.eugenandrei@gmail.com>
2023-07-04 19:41:42 +0300

    [commit 3 -eandrei][added components & navbar]

--- ip-tracker-app/src/app/app.module.ts
+++ ip-tracker-app/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { TableComponent } from './components/table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ ip-tracker-app/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent {
+
+  public ipAddressControl= new FormControl('')
+  
+}
